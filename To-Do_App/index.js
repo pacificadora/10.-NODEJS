271e9f2da7559169c3c17b0992dfdc5b11d18f69
pacificadora <singhalaman198@gmail.com>
2022-07-27 11:49:20 +0530

    install mongoose and nodemon

--- To-Do_App/index.js
+++ To-Do_App/index.js
@@ -1,1 +1,13 @@
 const express = require('express');
+const path = require('path');
+const port = 8000;
+const app = express();
+
+
+
+
+app.listen(port, function(err){
+    if(err){console.log("error in starting the server"); return;}
+
+    console.log("Yeah!! my server is running on port", port);
+})
